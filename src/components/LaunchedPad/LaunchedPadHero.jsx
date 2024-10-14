1efeb4eac76373987686809bf0b206ee7ec8fedc
ShaYri12 <sharjeelb60@gmail.com>
2024-10-14 23:33:47 +0500

    launchpad hero fix

--- src/components/LaunchedPad/LaunchedPadHero.jsx
+++ src/components/LaunchedPad/LaunchedPadHero.jsx
@@ -2,28 +2,27 @@ import React from "react";
 
 const LaunchedPadHero = () => {
   return (
-    <section className="min-h-screen bg-dark-blue text-white lg:px-[22px] md:pt-[80px] md:pt-[60px] pt-[50px] pb-[80px] pb-[29px] overflow-hidden">
-      <div className="max-w-[1280px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:h-[85vh] gap-[10px]">
+    <section className="bg-dark-blue text-white px-[22px] md:pt-[80px] md:pt-[60px] sm:pt-[40px] pt-[10px] pb-[80px] sm:pb-[40px] pb-[20.93px] overflow-hidden relative">
+      <img
+        src="/assets/launchpad-bg.png"
+        className="absolute top-0 left-0 w-full z-[1] object-cover h-full"
+      />
+
+      <div className="max-w-[1280px] mx-auto flex flex-col lg:flex-row justify-between items-center lg:h-[70vh] gap-[10px] relative z-[2] md:pt-0 pt-[20px]">
         {/* Left Section */}
-        <div className="text-left lg:max-w-[601px] px-[22px]">
-          <h1 className="md:text-[65px] text-[35px] md:leading-[74.74px] leading-[40.25px] font-[700] text-primaryColor">
+        <div className="text-left max-w-[979px] mx-auto text-center">
+          <h1 className="md:text-[65px] text-[31.27px] md:leading-[74.74px] leading-[35.96px] font-[700] text-primaryColor">
             Safest launchpad. multi-chain.
           </h1>
-          <div className="h-[3.04px] sm:h-[6px] w-[127px] sm:w-[251px] bg-primaryColor rounded-[20px] mt-[7px] md:mt-[14px]"></div>
-          <p className="md:mt-[14px] mt-[7px] text-[9.11px] md:text-[18px] text-white">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua...
+          <div className="h-[3.04px] sm:h-[6px] w-[127px] sm:w-[251px] bg-primaryColor rounded-[20px] mt-[7px] md:mt-[14px] mx-auto"></div>
+          <p className="md:mt-[14px] mt-[7px] text-[8.66px] md:text-[18px] text-white max-w-[801px] mx-auto">
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content
           </p>
-          <button className="px-[35.5px] py-[15px] md:text-[18px] text-[9.11px] font-[400] rounded-full border border-primaryColor text-primaryColor md:mt-[42px] mt-[21px]">
+          <button className="px-[16.93px] md:px-[35.5px] py-[7.22px] md:py-[15px] md:text-[18px] text-[9.11px] font-[400] rounded-full border border-primaryColor text-primaryColor md:mt-[42px] mt-[21px]">
             Connect Wallet
           </button>
-        </div>
-
-        <div className="lg:w-1/2 md:w-[70%] w-full">
-          <img
-            src="/assets/launchpad-hero.png"
-            className="h-full w-full object-cover"
-          />
         </div>
       </div>
     </section>
