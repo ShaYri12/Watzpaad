136f9638e3459330ca1818b83d0c41feb2820575
ShaYri12 <sharjeelb60@gmail.com>
2024-10-19 06:15:52 +0500

    win page done

--- src/pages/Stacking.jsx
+++ src/pages/Stacking.jsx
@@ -83,87 +83,89 @@ const Stacking = () => {
                 key={card.id}
                 className="bg-[#1F2835CC] rounded-[13.09px] border-[2.6px] border-[#303945] flex flex-col sm:p-[11px] p-[9.28px]"
               >
-                <div className="rounded-[6.55px] border border-primaryColor flex flex-col sm:gap-[28px] md:px-[12px] px-[10.12px] gap-[23.62px]">
-                  <div className="flex items-center justify-center pb-[28px] border-b border-b-[#676767] md:pt-[39px] pt-[33.73px] xl:px-[28px] px-[8px]">
-                    <div className="md:min-w-[90px] min-w-[75.91px] md:min-h-[90px] min-h-[75.91px]">
-                      <img
-                        src="/assets/icons/r-logo.png"
-                        alt=""
-                        className="md:w-[90px] w-[75.91px] md:h-[90px] h-[75.91px]"
-                      />
-                    </div>
-                    <div>
-                      <h2 className="text-primaryColor text-[23.29px] md:text-[27.61px] md:leading-[31.75px] font-[700]">
-                        Stake RenQ
-                      </h2>
-                      <p className="text-[#798DA3] text-[15.18px] md:text-[18px]">
-                        Stake RenQ - {card.time}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col sm:gap-[28px] gap-[23.62px] sm:px-[25px] px-[15px] md:py-[7px]">
-                    <div
-                      className="bg-[#1F2835CC] md:rounded-[10px] rounded-[8.43px] md:px-[22.5px] px-[18.91px] md:py-[17px] py-[15.05px]"
-                      style={{
-                        boxShadow:
-                          "-3.33px 3.33px 3.33px 0px #FFFFFF63 inset, 2.22px -2.22px 22.08px 0px #D6D6D62B inset",
-                        backdropFilter: "blur(82.17px)",
-                      }}
-                    >
-                      <div className="flex justify-between text-[#798DA3] md:text-[18px] md:leading-[20.7px] w-full font-[400]">
-                        <span>{t("apy")}</span>
-                        <span>{card.apy}</span>
+                <div className="card-border p-[1.96px] w-full">
+                  <div className="rounded-[6.55px] flex flex-col sm:gap-[28px] md:px-[12px] px-[10.12px] gap-[23.62px] relative z-[2]">
+                    <div className="flex items-center justify-center pb-[28px] border-b border-b-[#676767] md:pt-[39px] pt-[33.73px] xl:px-[28px] px-[8px]">
+                      <div className="md:min-w-[90px] min-w-[75.91px] md:min-h-[90px] min-h-[75.91px]">
+                        <img
+                          src="/assets/icons/r-logo.png"
+                          alt=""
+                          className="md:w-[90px] w-[75.91px] md:h-[90px] h-[75.91px]"
+                        />
                       </div>
-                      <div className="flex justify-between text-[#798DA3] w-full font-[400]">
-                        <span>{t("time")}</span>
-                        <span>{card.time}</span>
-                      </div>
-                    </div>
-
-                    <div className="flex gap-1 justify-between items-center">
-                      <div className="">
-                        <p className="text-[#798DA3] text-[15.18px] md:text-[18px] md:leading-[20.7px]">
-                          {t("stakedRenQ")}
-                        </p>
-                        <p className="text-primaryColor text-[23.29px] md:text-[27.61px] md:leading-[31.75px] font-[700]">
-                          {renQ} RenQ
+                      <div>
+                        <h2 className="text-primaryColor text-[23.29px] md:text-[27.61px] md:leading-[31.75px] font-[700]">
+                          Stake RenQ
+                        </h2>
+                        <p className="text-[#798DA3] text-[15.18px] md:text-[18px]">
+                          Stake RenQ - {card.time}
                         </p>
                       </div>
+                    </div>
 
-                      <div className="flex flex-col items-center gap-[12.65px] md:gap-[15px] md:w-[109px] w-[91.93px]">
-                        <div className="flex gap-2 w-full justify-between items-center">
+                    <div className="flex flex-col sm:gap-[28px] gap-[23.62px] sm:px-[25px] px-[15px] md:py-[7px]">
+                      <div
+                        className="bg-[#1F2835CC] md:rounded-[10px] rounded-[8.43px] md:px-[22.5px] px-[18.91px] md:py-[17px] py-[15.05px]"
+                        style={{
+                          boxShadow:
+                            "-3.33px 3.33px 3.33px 0px #FFFFFF63 inset, 2.22px -2.22px 22.08px 0px #D6D6D62B inset",
+                          backdropFilter: "blur(82.17px)",
+                        }}
+                      >
+                        <div className="flex justify-between text-[#798DA3] md:text-[18px] md:leading-[20.7px] w-full font-[400]">
+                          <span>{t("apy")}</span>
+                          <span>{card.apy}</span>
+                        </div>
+                        <div className="flex justify-between text-[#798DA3] w-full font-[400]">
+                          <span>{t("time")}</span>
+                          <span>{card.time}</span>
+                        </div>
+                      </div>
+
+                      <div className="flex gap-1 justify-between items-center">
+                        <div className="">
+                          <p className="text-[#798DA3] text-[15.18px] md:text-[18px] md:leading-[20.7px]">
+                            {t("stakedRenQ")}
+                          </p>
+                          <p className="text-primaryColor text-[23.29px] md:text-[27.61px] md:leading-[31.75px] font-[700]">
+                            {renQ} RenQ
+                          </p>
+                        </div>
+
+                        <div className="flex flex-col items-center gap-[12.65px] md:gap-[15px] md:w-[109px] w-[91.93px]">
+                          <div className="flex gap-2 w-full justify-between items-center">
+                            <button
+                              onClick={() => setRenQ(renQ + 1)}
+                              className="md:w-[37.7px] md:min-w-[37.7px] w-[31.79px]  max-h-[37.7px] md:min-h-[37.7px] min-h-[31.79px] h-[31.79px] bg-primaryColor rounded-full flex items-center justify-center"
+                            >
+                              <GoPlus className="text-black text-[20px]" />
+                            </button>
+                            <button
+                              onClick={handleRenQMinus}
+                              className="md:w-[37.7px] md:min-w-[37.7px] w-[31.79px] max-h-[37.7px] md:min-h-[37.7px] min-h-[31.79px] h-[31.79px] bg-[#878787] rounded-full flex items-center justify-center"
+                            >
+                              <LuMinus className="text-white text-[18px]" />
+                            </button>
+                          </div>
                           <button
-                            onClick={() => setRenQ(renQ + 1)}
-                            className="md:w-[37.7px] md:min-w-[37.7px] w-[31.79px]  max-h-[37.7px] md:min-h-[37.7px] min-h-[31.79px] h-[31.79px] bg-primaryColor rounded-full flex items-center justify-center"
+                            className="bg-primaryColor text-black text-[12px] md:text-[14px] font-[400] py-3 px-[28px] w-full rounded-full"
+                            style={{
+                              boxShadow: "0px 2.85px 16.1px 0px #38DCC88C",
+                            }}
                           >
-                            <GoPlus className="text-black text-[20px]" />
-                          </button>
-                          <button
-                            onClick={handleRenQMinus}
-                            className="md:w-[37.7px] md:min-w-[37.7px] w-[31.79px] max-h-[37.7px] md:min-h-[37.7px] min-h-[31.79px] h-[31.79px] bg-[#878787] rounded-full flex items-center justify-center"
-                          >
-                            <LuMinus className="text-white text-[18px]" />
+                            {t("claim")}
                           </button>
                         </div>
-                        <button
-                          className="bg-primaryColor text-black text-[12px] md:text-[14px] font-[400] py-3 px-[28px] w-full rounded-full"
-                          style={{
-                            boxShadow: "0px 2.85px 16.1px 0px #38DCC88C",
-                          }}
-                        >
-                          {t("claim")}
-                        </button>
                       </div>
-                    </div>
 
-                    <div className="flex gap-1 justify-between items-center md:pb-[39px] pb-[33.73px]">
-                      <p className="text-[#798DA3] text-[15.18px] md:text-[18px]">
-                        {t("totalStaked")}
-                      </p>
-                      <p className="text-primaryColor text-[15.18px] md:text-[18px]">
-                        {card.totalStacked}
-                      </p>
+                      <div className="flex gap-1 justify-between items-center md:pb-[39px] pb-[33.73px]">
+                        <p className="text-[#798DA3] text-[15.18px] md:text-[18px]">
+                          {t("totalStaked")}
+                        </p>
+                        <p className="text-primaryColor text-[15.18px] md:text-[18px]">
+                          {card.totalStacked}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
