73130cb0d6c9d46b84579e55036654d5fc35ba6a
ShaYri12 <sharjeelb60@gmail.com>
2024-10-19 05:51:54 +0500

    market pages broder added

--- src/pages/TradingMarket.jsx
+++ src/pages/TradingMarket.jsx
@@ -38,12 +38,12 @@ export default function TradingMarket({ dashboard = false }) {
         "px-[22px] md:pb-[100px] pb-[62px] md:pt-[160px] pt-[90px]"
       }`}
     >
-      {/* <img
-        src="/assets/marketplace-bg-shade.png"
+      <img
+        src="/assets/trading-market-bg-shade.png"
         className={`w-full h-full absolute top-0 left-0 z-[1]${
           dashboard && "hidden"
         }]`}
-      /> */}
+      />
       <div className="relative z-20 max-w-[1280px] mx-auto">
         <div
           className={` ${dashboard ? "mb:mb-[43px] mb-[39px]" : "mb-[43px]"}`}
@@ -64,79 +64,81 @@ export default function TradingMarket({ dashboard = false }) {
           } md:gap-[19px] gap-[38px]`}
         >
           <div className="bg-[#1F2835CC] rounded-[26.71px] border-[2.6px] border-[#303945] flex md:px-[15px] px-[13px] md:py-[16px] py-[9px]  items-center flex-1">
-            <div
-              className={`md:px-[18px] md:py-[32px] py-[39.28px] px-[14px] md:rounded-[10.39px] rounded-[13.35px] border border-primaryColor flex lg:flex-row flex-col flex-1 lg:items-center w-full h-full ${
-                dashboard
-                  ? "md:gap-[20px] gap-[21px]"
-                  : "xl:gap-[51px] md:gap-[40px] gap-[23px]"
-              }`}
-            >
+            <div className="md:card-border-wide card-border w-full p-[2px]">
               <div
-                className={`rounded-[10.39px] flex items-center justify-center h-[198.18px] md:h-[249.34px] md:max-w-[249.34px] max-w-[198.18px] w-full overflow-hidden ${
-                  !dashboard && "xl:ms-[50px]"
-                } border-[2.6px] border-[#CAF813]`}
+                className={`md:px-[18px] md:py-[32px] py-[39.28px] px-[14px] md:rounded-[10.39px] rounded-[13.35px] flex lg:flex-row flex-col flex-1 lg:items-center w-full h-full relative z-[2]  ${
+                  dashboard
+                    ? "md:gap-[20px] gap-[21px]"
+                    : "xl:gap-[51px] md:gap-[40px] gap-[23px]"
+                }`}
               >
-                <img
-                  src="/assets/marketplace-card.png"
-                  alt="Pixel Cat"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <div className={`flex-1 ${!dashboard && "xl:pe-[50px]"}`}>
-                <h2 className="text-[23.84px] md:text-[30px] md:leading-[34.5px] leading-[27.42px] text-white font-[700] mb-[4px]">
-                  #{item.id}
-                </h2>
-                <p className="text-[15.9px] md:text-[20px] md:leading-[23px] leading-[18.28px] text-[#798DA3]">
-                  Shiba Dev
-                </p>
-                <div className="mb-[15px] mt-[15px] pb-[15px] border-b border-white">
-                  <p className="text-[14px] md:text-[18px] text-[#798DA3] md:mb-[8px] gap-[6.36px]">
-                    Grade
-                  </p>
-                  <div className="flex md:gap-[3.40px] gap-[2.76px]">
-                    {[...Array(5)].map((_, i) => (
-                      <FaStar
-                        key={i}
-                        className="text-[#FAFF00] md:text-[18px] text-[16px]"
-                      />
-                    ))}
-                  </div>
-                </div>
-                <div className="flex justify-between w-full items-center gap-2">
-                  <div className="flex flex-col items-start">
-                    <p className="text-[14.31px] md:text-[18px] text-[#798DA3] md:leading-[20.7px] leading-[16.45px]">
-                      Sale Price
-                    </p>
-                    <p className="flex items-center text-[23.84px] md:text-[30px] md:leading-[34.5px] leading-[27.42px] font-[700]">
-                      <span className="text-white">
-                        {parseFloat(item.price) % 1 === 0
-                          ? `${parseFloat(item.price).toFixed(1)}`
-                          : item.price}
-                      </span>
-                      <span className="text-primaryColor">BNB</span>
-                    </p>
-                    <p className="text-[#798DA3] text-[14.31px] md:text-[18px] md:leading-[20.7px] leading-[16.45px]">
-                      ≈${parseFloat(item.salePrice)}
-                    </p>
-                  </div>
-                  <button className="flex w-[46px] h-[46px] min-w-[46px] min-h-[46px] rounded-[10px] bg-primaryColor items-center justify-center">
-                    <img
-                      src="/assets/icons/exchange.png"
-                      alt=""
-                      className="w-[28px] h-[28px]"
-                    />
-                  </button>
-                </div>
                 <div
-                  className={`md:mt-[16px] mt-[16px] ${
-                    dashboard
-                      ? "xl:px-[30px] sm:px-[20px]"
-                      : "xl:px-[30px] sm:px-[20px]"
-                  }`}
+                  className={`rounded-[10.39px] flex items-center justify-center h-[198.18px] md:h-[249.34px] md:max-w-[249.34px] max-w-[198.18px] w-full overflow-hidden ${
+                    !dashboard && "xl:ms-[50px]"
+                  } border-[2.6px] border-[#CAF813]`}
                 >
-                  <button className="px-[16.93px] md:px-[35.5px] py-[11.92px] md:py-[15px] md:text-[18px] text-[14.31px] font-[400] rounded-full border border-primaryColor text-primaryColor w-full">
-                    Connect Wallet
-                  </button>
+                  <img
+                    src="/assets/marketplace-card.png"
+                    alt="Pixel Cat"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className={`flex-1 ${!dashboard && "xl:pe-[50px]"}`}>
+                  <h2 className="text-[23.84px] md:text-[30px] md:leading-[34.5px] leading-[27.42px] text-white font-[700] mb-[4px]">
+                    #{item.id}
+                  </h2>
+                  <p className="text-[15.9px] md:text-[20px] md:leading-[23px] leading-[18.28px] text-[#798DA3]">
+                    Shiba Dev
+                  </p>
+                  <div className="mb-[15px] mt-[15px] pb-[15px] border-b border-white">
+                    <p className="text-[14px] md:text-[18px] text-[#798DA3] md:mb-[8px] gap-[6.36px]">
+                      Grade
+                    </p>
+                    <div className="flex md:gap-[3.40px] gap-[2.76px]">
+                      {[...Array(5)].map((_, i) => (
+                        <FaStar
+                          key={i}
+                          className="text-[#FAFF00] md:text-[18px] text-[16px]"
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex justify-between w-full items-center gap-2">
+                    <div className="flex flex-col items-start">
+                      <p className="text-[14.31px] md:text-[18px] text-[#798DA3] md:leading-[20.7px] leading-[16.45px]">
+                        Sale Price
+                      </p>
+                      <p className="flex items-center text-[23.84px] md:text-[30px] md:leading-[34.5px] leading-[27.42px] font-[700]">
+                        <span className="text-white">
+                          {parseFloat(item.price) % 1 === 0
+                            ? `${parseFloat(item.price).toFixed(1)}`
+                            : item.price}
+                        </span>
+                        <span className="text-primaryColor">BNB</span>
+                      </p>
+                      <p className="text-[#798DA3] text-[14.31px] md:text-[18px] md:leading-[20.7px] leading-[16.45px]">
+                        ≈${parseFloat(item.salePrice)}
+                      </p>
+                    </div>
+                    <button className="flex w-[46px] h-[46px] min-w-[46px] min-h-[46px] rounded-[10px] bg-primaryColor items-center justify-center">
+                      <img
+                        src="/assets/icons/exchange.png"
+                        alt=""
+                        className="w-[28px] h-[28px]"
+                      />
+                    </button>
+                  </div>
+                  <div
+                    className={`md:mt-[16px] mt-[16px] ${
+                      dashboard
+                        ? "xl:px-[30px] sm:px-[20px]"
+                        : "xl:px-[30px] sm:px-[20px]"
+                    }`}
+                  >
+                    <button className="px-[16.93px] md:px-[35.5px] py-[11.92px] md:py-[15px] md:text-[18px] text-[14.31px] font-[400] rounded-full border border-primaryColor text-primaryColor w-full">
+                      Connect Wallet
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -147,50 +149,52 @@ export default function TradingMarket({ dashboard = false }) {
               dashboard ? "lg:w-[34%]" : "md:w-[34%]"
             }`}
           >
-            <div className="md:px-[18px] md:py-[32px] py-[40px] px-[14px] md:rounded-[10.39px] rounded-[13.35px] border border-primaryColor flex flex-col flex-1 w-full h-full md:gap-[31px] gap-[24.55px]">
-              <h2 className="text-[24.36px] md:text-[30px] font-[700] text-white">
-                On-Chain Data
-              </h2>
-              <div className="flex flex-col md:gap-[20px] gap-[16.24px]">
-                <div className="flex justify-between w-full flex-wrap gap-1">
-                  <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
-                    Owner Address
-                  </p>
-                  <p className="text-primaryColor text-[14.62px] md:text-[18px]">
-                    0x9F496...2D570eE
-                  </p>
-                </div>
-                <div className="flex justify-between w-full flex-wrap gap-1">
-                  <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
-                    Contract Address
-                  </p>
-                  <p className="text-primaryColor text-[14.62px] md:text-[18px]">
-                    0x9F496...2D570eE
-                  </p>
-                </div>
-                <div className="flex justify-between w-full flex-wrap gap-1">
-                  <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
-                    Mining Power
-                  </p>
-                  <p className="text-primaryColor text-[14.62px] md:text-[18px]">
-                    1.0 BNB
-                  </p>
-                </div>
-                <div className="flex justify-between w-full flex-wrap gap-1">
-                  <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
-                    Decompose Status
-                  </p>
-                  <p className="text-primaryColor text-[14.62px] md:text-[18px]">
-                    Locked
-                  </p>
-                </div>
-                <div className="flex justify-between w-full flex-wrap gap-1">
-                  <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
-                    Decomposable In
-                  </p>
-                  <p className="text-primaryColor text-[14.62px] md:text-[18px]">
-                    26:14:34:41
-                  </p>
+            <div className="lg:card-border md:card-border-wide card-border-wide w-full h-full p-[2px]">
+              <div className="md:px-[18px] md:py-[32px] py-[40px] px-[14px] md:rounded-[10.39px] rounded-[13.35px] flex flex-col flex-1 w-full h-full md:gap-[31px] gap-[24.55px] relative z-[2]">
+                <h2 className="text-[24.36px] md:text-[30px] font-[700] text-white">
+                  On-Chain Data
+                </h2>
+                <div className="flex flex-col md:gap-[20px] gap-[16.24px]">
+                  <div className="flex justify-between w-full flex-wrap gap-1">
+                    <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
+                      Owner Address
+                    </p>
+                    <p className="text-primaryColor text-[14.62px] md:text-[18px]">
+                      0x9F496...2D570eE
+                    </p>
+                  </div>
+                  <div className="flex justify-between w-full flex-wrap gap-1">
+                    <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
+                      Contract Address
+                    </p>
+                    <p className="text-primaryColor text-[14.62px] md:text-[18px]">
+                      0x9F496...2D570eE
+                    </p>
+                  </div>
+                  <div className="flex justify-between w-full flex-wrap gap-1">
+                    <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
+                      Mining Power
+                    </p>
+                    <p className="text-primaryColor text-[14.62px] md:text-[18px]">
+                      1.0 BNB
+                    </p>
+                  </div>
+                  <div className="flex justify-between w-full flex-wrap gap-1">
+                    <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
+                      Decompose Status
+                    </p>
+                    <p className="text-primaryColor text-[14.62px] md:text-[18px]">
+                      Locked
+                    </p>
+                  </div>
+                  <div className="flex justify-between w-full flex-wrap gap-1">
+                    <p className="text-[14.62px] md:text-[18px] text-[#798DA3]">
+                      Decomposable In
+                    </p>
+                    <p className="text-primaryColor text-[14.62px] md:text-[18px]">
+                      26:14:34:41
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -218,11 +222,15 @@ export default function TradingMarket({ dashboard = false }) {
             Market Rules
           </h4>{" "}
           <p className="text-[16.69px] md:text-[18px] font-[400] text-[#798DA3]">
-            1.NFTs can be purchased with $BBT or Any other Choice of  Coins as per users listing criteria.
+            1.NFTs can be purchased with $BBT or Any other Choice of Coins as
+            per users listing criteria.
             <br />
-            2.After NFT is listed in the trading market, operations such as transfer, auction, and staking are not allowed.
+            2.After NFT is listed in the trading market, operations such as
+            transfer, auction, and staking are not allowed.
             <br />
-            3.The market will charge 15% of the seller's revenue as a service fee, of which 5% is send to original Minter. 5% Will transfer to the Reward to all NFT holders 5% to the Marketing Wallet.
+            3.The market will charge 15% of the seller's revenue as a service
+            fee, of which 5% is send to original Minter. 5% Will transfer to the
+            Reward to all NFT holders 5% to the Marketing Wallet.
           </p>
         </div>
 
