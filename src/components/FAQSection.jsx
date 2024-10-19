3b9218f98d436e97d679304dd305326d2c42a38c
ShaYri12 <sharjeelb60@gmail.com>
2024-10-19 16:48:05 +0500

    faq border fix

--- src/components/FAQSection.jsx
+++ src/components/FAQSection.jsx
@@ -8,17 +8,18 @@ const FAQItem = ({ item }) => {
 
   return (
     <div className="mb-[19px]">
-      <button
-        className="w-full text-left px-[18.22px] md:px-[48px] py-[16.5px] bg-[#121C29] rounded-[13.34px] flex justify-between items-center focus:outline-none focus:ring-1 focus:ring-teal-500 gap-1"
-        onClick={() => setIsOpen(!isOpen)}
-        style={{
-          backdropFilter: "blur(33.34968948364258px)",
-        }}
-      >
-        <span className="text-[#798DA3] md:text-[18px] text-[14.04px]">
-          {t(item.question)}  {/* Using translation */}
-        </span>
-        
+      <button className="card-border-faq w-full p-[2px]">
+        <div
+          className="w-full text-left px-[18.22px] md:px-[48px] py-[16.5px] bg-[#121C29] rounded-[13.34px] flex justify-between items-center focus:outline-none focus:ring-1 focus:ring-teal-500 gap-1"
+          onClick={() => setIsOpen(!isOpen)}
+          style={{
+            backdropFilter: "blur(33.34968948364258px)",
+          }}
+        >
+          <span className="text-[#798DA3] md:text-[18px] text-[14.04px]">
+            {t(item.question)} {/* Using translation */}
+          </span>
+
           <div
             className={`md:w-[40.95px] md:h-[40.95px] md:min-w-[40.95px] md:min-h-[40.95px] w-[24.07px] h-[24.07px] min-w-[24.07px] min-h-[24.07px] flex items-center justify-center text-[#1D1D23] rounded-[6.02px] md:rounded-[10.24px] ${
               isOpen ? "bg-white" : "bg-primaryColor"
@@ -33,6 +34,7 @@ const FAQItem = ({ item }) => {
               <FiPlus className="md:w-[20px] w-[15px] md:h-[20px] h-[15px]" />
             )}
           </div>
+        </div>
       </button>
       <div
         className={`overflow-hidden transition-all duration-500 ease-in-out ${
@@ -40,7 +42,7 @@ const FAQItem = ({ item }) => {
         }`}
       >
         <div className="mt-2 px-[50px] py-4 bg-[#1f2c40] rounded-lg text-gray-300 md:text-[16px] text-[14.04px]">
-          {t(item.answer)}  {/* Using translation */}
+          {t(item.answer)} {/* Using translation */}
         </div>
       </div>
     </div>
